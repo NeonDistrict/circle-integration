ab99d9c5318022bf9b98c3df0d191f1139d25e27
Adrian Seeley <contact@adrianseeley.com>
2021-12-27 13:35:08 +0000

    test amounts as sale items

--- src/sale_items.dev.js
+++ src/sale_items.dev.js
@@ -1,4 +1,6 @@
-module.exports = sale_items_dev = [
+const test_amounts = require('./test/test_amounts.js');
+
+const sale_items_dev = [
     {
         "sale_item_key": "NEON_1000",
         "currency": "USD",
@@ -6,21 +8,19 @@ module.exports = sale_items_dev = [
         "statement_description": "NEON DISTRICT: 1000 NEON",
         "store_description": "Adds 1000 NEON to your account.",
         "store_image": "https://images/NEON_1000.png"
-    },
-    {
-        "sale_item_key": "NEON_5000",
-        "currency": "USD",
-        "amount": "5.00",
-        "statement_description": "NEON DISTRICT: 5000 NEON",
-        "store_description": "Adds 5000 NEON to your account.",
-        "store_image": "https://images/NEON_5000.png"
-    },
-    {
-        "sale_item_key": "NEON_20000",
-        "currency": "USD",
-        "amount": "20.00",
-        "statement_description": "NEON DISTRICT: 20000 NEON",
-        "store_description": "Adds 20000 NEON to your account.",
-        "store_image": "https://images/NEON_20000.png"
     }
 ];
+
+for (const test_amount_key in test_amounts) {
+    test_amount = test_amounts[test_amount_key];
+    sale_items_dev.push({
+        "sale_item_key": test_amount_key,
+        "currency": "USD",
+        "amount": test_amount.amount,
+        "statement_description": `NEON DISTRICT: ${test_amount_key}`,
+        "store_description": test_amount.description,
+        "store_image": `https://images/${test_amount_key}.png`
+    });
+}
+
+module.exports = sale_items_dev;
--- src/test/test.js
+++ src/test/test.js
@@ -3,6 +3,24 @@ const server = require('../server.js');
 const circle_integration_client = require('../circle_integration_client.js');
 const config_dev = require('../config.dev.js');
 const test_cards = require('./test_cards.js');
+const test_amounts = require('./test_amounts.js');
+
+const ok_purchase = {
+    card_number: test_card.card_number,
+    cvv: 123, 
+    name: 'Theo Banks',
+    city: 'Toronto',
+    country: 'CA',
+    address_1: '420 Pickadilly Lane',
+    address_2: '',
+    district: 'ON',
+    postal: 'M4C 2K2',
+    expiry_month: 12,
+    expiry_year: 2024,
+    email: 'theobanks@neondistrict.io',
+    phone: '+16132134512',
+    sale_item_key: 'NEON_1000'
+};
 
 describe('circle-integration-server', function () {
     before(function (done) {
@@ -25,27 +43,24 @@ describe('circle-integration-server', function () {
     });
 
     it('make a purchase', async function () {
-        const test_card = test_cards[0];
-
-        const idempotency_key = circle_integration_client.generate_idempotency_key();
         const purchase_result = await circle_integration_client.purchase(
-            idempotency_key,
-            test_card.card_number,
-            123,
-            'Theo Banks',
-            'Toronto',
-            'CA',
-            '420 Pickadilly Lane',
-            '',
-            'ON',
-            'M4C 2K2',
-            12,
-            2024,
-            'theobanks@neondistrict.io',
-            '+16132134512',
-            'NEON_1000'
+            circle_integration_client.generate_idempotency_key(),
+            ok_purchase.card_number,
+            ok_purchase.cvv,
+            ok_purchase.name,
+            ok_purchase.city,
+            ok_purchase.country,
+            ok_purchase.address_1,
+            ok_purchase.address_2,
+            ok_purchase.district,
+            ok_purchase.postal,
+            ok_purchase.expiry_month,
+            ok_purchase.expiry_year,
+            ok_purchase.email,
+            ok_purchase.phone,
+            ok_purchase.sale_item_key
         );
-        console.log('purchase result');
-        console.log(purchase_result);
+        assert(purchase_result.status === 'confirmed');
+
     });
 });
