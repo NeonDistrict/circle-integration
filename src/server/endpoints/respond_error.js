a8f41ecf56a3a3ae498c74f88013c9e82d6882c8
Adrian Seeley <aseeley@neondistrict.io>
2022-03-03 16:57:22 +0000

    respond error properly logs message and stack now

--- src/server/endpoints/respond_error.js
+++ src/server/endpoints/respond_error.js
@@ -3,7 +3,8 @@ const log = require('../utilities/log.js');
 module.exports = (res, error) => {
     log({
         event: 'responding error',
-        error: error
+        error: error.message,
+        stack: error.stack
     });
     res.status(500);
     res.send({
